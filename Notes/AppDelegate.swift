65e51c82878d500af83181bcb1c13fb8a7241875
Perry Gabriel <prgabriel88@gmail.com>
2016-11-18 22:32:19 -0500

    Initial Commit

---
+++ Notes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Notes
+//
+//  Created by Perry Gabriel on 11/18/16.
+//  Copyright © 2016 Perry R. Gabriel. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+**Notes**
+
+Notes is designed to create notes and gain the ability to add files to individual
+notes. 
